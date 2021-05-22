81667b8abbb8c78ef8695001a6327cacfd5c4118
MaksimG20 <garinmaksm@gmail.com>
2021-05-22 12:10:58 +0300

    Lab8

---
+++ Task08/public/index.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Lab8</title>
+    </head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="jquery-3.5.1.min.js"></script>
+    <script src="ajax.js"></script>
+    <body>
+        <div>
+            <a style href="add_student.php">Add to database</a>
+            <hr>
+            <a style href="exam_info.php">Add exam results</a>
+        </div>
+    </body>
+</html>
